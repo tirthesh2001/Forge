c671b53e64606b919ab03b0b848175f120a2d30c
tirthesh2001 <tirthesh18@gmail.com>
2026-04-06 13:55:20 +0530

    one more fix wrt to imports

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Outlet, useNavigate, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import Sidebar from './Sidebar'
 import CommandPalette from './CommandPalette'
 
